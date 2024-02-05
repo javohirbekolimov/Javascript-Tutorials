39f0c4b4a75e434a4defe3cbebea3c072b9a4695
javohirbekolimov <javohirbekolimov01@gmail.com>
2024-02-05 09:22:19 +0300

    first commit

---
+++ Java script-2/app.js
@@ -0,0 +1,12 @@
+"Use strict" ;
+
+// const here = confirm(`Ismingiz nima`)
+// let yosh = 
+
+// const number = Number ( prompt ( "siz nechchi yilsiz"))
+// let yosh = 2024 - number ;
+// console.log(`siz ${number} yoshdasiz`);
+
+const number1 =  Number( prompt (`son kiriting`));
+const  number2 = Number( prompt (`darajasini kiriting`));
+console.log(number1  + number2 , number1 - number2 , number1  * number2 , number1 / number2 );
---
+++ Java script/js/app.js
@@ -0,0 +1,6 @@
+// consolle.log('javohir') ;
+
+let x = 5 ;
+let y = 4 ;
+
+ console.log(  x + y , x - y ,x * y , x / y , firstName  )
